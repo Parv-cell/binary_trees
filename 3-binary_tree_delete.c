23e61ff56ee5c387440fff3579345d9f08d3a319
Kendu <kenduajayi@gmail.com>
2023-08-31 14:24:16 -0700

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,19 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_delete - this frees an entire binary tree
+*
+* Description:
+* @tree: pointer to root node of the tree to be deleted
+*/
+
 void binary_tree_delete(binary_tree_t *tree)
 {
+	if (!tree)
+		return;
 
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
